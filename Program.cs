61bece1232beb67db086c864b614922ea69998b1
zakyahk <zakyahk@icloud.com>
2023-03-25 14:14:42 +0700

    First Commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPMODUL6_1302210135
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
