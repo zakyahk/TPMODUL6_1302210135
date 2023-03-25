872d3b2163abdf9860b5a0ad9ce47c5927fb688c
zakyahk <zakyahk@icloud.com>
2023-03-25 14:15:53 +0700

    menambahkan class SayaTubeVideo

---
+++ SayaTubeVideo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPMODUL6_1302210135
+{
+    internal class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo(string title)
+        {
+            this.id = new Random().Next(10000, 99999);
+            this.title = title;
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int count)
+        {
+            playCount += count;
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine("ID: " + id);
+            Console.WriteLine("Title: " + title);
+            Console.WriteLine("Play count: " + playCount);
+        }
+
+        static void Main(string[] args)
+        {
+            SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract — [NAMA_PRAKTIKAN]");
+            video.IncreasePlayCount(10);
+            video.PrintVideoDetails();
+        }
+    }
+}
+
